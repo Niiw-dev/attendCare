fe0385eb22c6f71e5a4d075e4041d687a58fbed2
edwin.correa <edwin.correa@savne.net>
2026-05-06 11:50:17 -0500

    fix: Se agrega nginx a docker

--- docker/backend/entrypoint.sh
+++ docker/backend/entrypoint.sh
@@ -1,26 +1,33 @@
 #!/bin/sh
 
-echo "Esperando a PostgreSQL real..."
+echo "Esperando a PostgreSQL..."
 
+# Espera a que el puerto esté abierto
+until nc -z db 5432; do
+  echo "Postgres no está listo..."
+  sleep 2
+done
+
+echo "PostgreSQL accesible"
+
+# Espera conexión real
 until python -c "
-import psycopg2
-import os
-try:
-    psycopg2.connect(
-        dbname=os.environ.get('DB_NAME'),
-        user=os.environ.get('DB_USER'),
-        password=os.environ.get('DB_PASSWORD'),
-        host='db',
-        port=5432
-    )
-    print('DB OK')
-except Exception:
-    exit(1)
+import psycopg2, os
+psycopg2.connect(
+    dbname=os.environ.get('DB_NAME'),
+    user=os.environ.get('DB_USER'),
+    password=os.environ.get('DB_PASSWORD'),
+    host='db',
+    port=5432
+)
 "; do
+  echo "Esperando conexión real..."
   sleep 2
 done
 
 echo "Base de datos lista 🚀"
 
-python app/manage.py migrate
-python app/manage.py runserver 0.0.0.0:8000
+python manage.py migrate
+python manage.py collectstatic --noinput
+
+gunicorn core.wsgi:application --bind 0.0.0.0:8000
