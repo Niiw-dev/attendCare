a1396288a3633c6c3ab64a9d13e6e70b29b2231b
edwin.correa <edwin.correa@savne.net>
2026-05-07 10:53:36 -0500

    Se agrega el módulo de ministerio

--- app/core/urls.py
+++ app/core/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('no-access/', no_access_view),
     path('', include('users.urls')),
+    path('', include('ministries.urls')),
     path('', lambda request: redirect('/dashboard/')),
     re_path(r'^.*$', lambda request: redirect('/no-access/')),
 ]
---
+++ app/ministries/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from users.models import User
+
+
+class Ministry(models.Model):
+    name = models.CharField(max_length=150, unique=True)
+
+    description = models.TextField(
+        blank=True
+    )
+
+    leaderAssigned = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        limit_choices_to={'role': 'LIDER'}
+    )
+
+    isActive = models.BooleanField(
+        default=True
+    )
+
+    createdAt = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    def __str__(self):
+        return self.name
